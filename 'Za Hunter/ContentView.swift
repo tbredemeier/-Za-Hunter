de5e6f5971146a4df4c79469f623691732b2b6d9
Tom Bredemeier <tbredemeier@barrington220.org>
2021-03-08 19:42:53 -0600

    Initial Commit

---
+++ 'Za Hunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  'Za Hunter
+//
+//  Created by Tom Bredemeier on 3/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Tom Bredemeier on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
