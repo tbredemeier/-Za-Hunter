b6d70441d44c835da06b8ec5b2c917711d4772b8
Tom Bredemeier <tbredemeier@barrington220.org>
2024-02-16 15:15:53 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Tom Bredemeier on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
