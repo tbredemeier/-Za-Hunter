d926128c8439f0bbbc3438b81bfe62afa718da8a
Tom Bredemeier <tbredemeier@barrington220.org>
2026-02-17 10:51:32 -0600

    Added map search feature

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -12,17 +12,46 @@ struct ContentView: View {
     @State private var startPosition = MapCameraPosition.userLocation(fallback: .automatic)
     @State private var mapRegion = MKCoordinateRegion()
     @State private var locationManager = LocationManager()
+    @State private var places = [Place]()
     var body: some View {
         NavigationView {
             Map(position: $startPosition) {
                 UserAnnotation()
+                ForEach(places) { place in
+                    Annotation(place.mapItem.name!, coordinate: place.mapItem.placemark.coordinate) {
+                        Image("pizza")
+                    }
+                }
+            }
+            .onMapCameraChange { context in
+                mapRegion = context.region
+                performSearch(item: "Pizza")
             }
             .navigationBarTitle("'Za Hunter", displayMode: .inline)
             .toolbarBackground(.hidden, for: .navigationBar)
+        }
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {
+                places.removeAll()
+                for mapItem in response.mapItems {
+                    places.append(Place(mapItem: mapItem))
+                }
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let mapItem: MKMapItem
 }
